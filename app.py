22acb4f16e5e3b458d7efe88888bf07f01ece619
Abdullah Khan <abdullah@khan.com>
2025-02-12 13:38:38 +0300

    commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, jsonify
+import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get-time", methods=["GET"])
+def get_time():
+    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return jsonify({"time": current_time})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
